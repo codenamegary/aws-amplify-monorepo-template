7cd3888b2897632f4da20e648895602b7831335b
Gary Saunders <gary.saunders@sportsengine.com>
2021-04-30 14:43:15 -0400

    Added backend UI.

---
+++ backend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Backend</h1>
+        <h3>The AWS Amplify Monorepo Template</h3>
+        <p>Put your API documentation and other relevant things here.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
